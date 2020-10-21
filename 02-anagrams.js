e2b74c7761358221607c9cef0a14b2264ce4e31f
Greg Gardini <ggard214@gmail.com>
2020-10-21 14:41:06 -0700

    First Anagram

--- 02-anagrams.js
+++ 02-anagrams.js
@@ -1,7 +1,23 @@
 function firstAnagram(str1, str2) {
-  // Code goes here ....
-}
+  let array1 = str1.split("");
+  let array2 = str2.split("");
 
+  for (let i = 0; i < array1.length; i++) {
+    let index = array2.indexOf(array1[i]);
+    if (index >= 0) {
+    array2.splice(index, 1);
+    }
+  }
+  console.log(array2, array1);
+
+  if (array2.length > 0) {
+    return false;
+  } else {
+    return true;
+  }
+  }
+console.log(firstAnagram("gizmo", "sally"));    // => false
+console.log(firstAnagram("elvis", "lives"));    // => true
 
 
 function secondAnagram(str1, str2) {
